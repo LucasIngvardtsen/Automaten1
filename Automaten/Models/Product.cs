8752456db6c3710e11ab41d5b9da0a9fabba52e2
lingv <lingv@LUCAS>
2025-08-28 09:19:05 +0200

    Add models classes

---
+++ Automaten/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaten.Models
+{
+    public class Product
+    {
+
+        private static int _staticID = 1;
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(string name, int price, int stock)
+        {
+            Name = name;
+            Price = price;
+            Stock = stock;
+        }
+    }
+}
